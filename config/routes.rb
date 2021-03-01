719d1f6421cf31c9cb425ce80ee16e8b82494d46
Max Keller <max@klauskeller.com>
2021-03-01 22:16:01 +0100

    people#new form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'people/create'
-  get 'people/edit'
-  get 'people/update'
   root to: 'groups#index'
   devise_for :users
+  resources :people, only: [ :edit, :update, :show, :create]
   resources :groups, only: [ :index, :create, :show ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
