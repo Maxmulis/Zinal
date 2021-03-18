e25db284dea4078d7f2fce4e84bf500fa51c2cb1
Max Keller <max@klauskeller.com>
2021-03-18 17:55:40 +0100

    person#edit working

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,5 +1,6 @@
 class PeopleController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_person, only: [ :show, :edit, :update ]
   def index
     @people = Person.includes(:group).sort_by { |person| person.group.name }
   end
@@ -10,17 +11,19 @@ class PeopleController < ApplicationController
   end
 
   def show
-    @person = Person.find(params[:id])
   end
 
   def create
     @person = Person.new(person_params)
     group = Group.find(params[:group_id])
     @person.group = group
-    if @person.save
-      redirect_to groups_path(query: group.name)
-    else
-      render :new
+    respond_to do |format|
+      if @person.save
+        format.html { redirect_to group_people_path(group) }
+        #format.turbo_stream { render turbo_stream: turbo_stream.prepend(group, partial: 'people/person', locals: { person: @person })}
+      else
+        render :new
+      end
     end
   end
 
@@ -28,6 +31,13 @@ class PeopleController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+      if @person.update(person_params)
+        format.html { render :show, notice: "#{@person.firstname.capitalize} wurde aktualisiert!"}
+      else
+        format.html { render :edit, alert: @person.errors.full_messages }
+      end
+    end
   end
 
   def people
@@ -36,6 +46,10 @@ class PeopleController < ApplicationController
   end
 
   private
+
+  def set_person
+    @person = Person.find(params[:id])
+  end
 
   def person_params
     params.require(:person).permit(:firstname, :phone, :comment, :birthdate)
